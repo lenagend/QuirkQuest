e58c69798e2e48b46539d439d22057a1c8f4aa1e
lenagend <57205046+lenagend@users.noreply.github.com>
2023-11-24 09:24:33 +0900

    당첨번호 리턴하는 엔트포인트 /api/lotto 완료

---
+++ src/main/java/com/quirkventure/quirkquestweb/lotto/controller/LottoDrawsController.java
@@ -0,0 +1,27 @@
+package com.quirkventure.quirkquestweb.lotto.controller;
+
+import com.quirkventure.quirkquestweb.lotto.entity.LottoDraws;
+import com.quirkventure.quirkquestweb.lotto.service.LottoDrawsService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/api/lotto")
+public class LottoDrawsController {
+
+    private final LottoDrawsService lottoDrawsService;
+
+    public LottoDrawsController(LottoDrawsService lottoDrawsService) {
+        this.lottoDrawsService = lottoDrawsService;
+    }
+
+    @GetMapping
+    public Page<LottoDraws> getAllLottoDraws(Pageable pageable) {
+        return lottoDrawsService.findAllLottoDraws(pageable);
+    }
+}
